d37b941ae0c2840a43fb6158b9f88e609c704edb
Nikita Haidai <90444574+fp0x@users.noreply.github.com>
2021-09-28 13:52:30 +0300

    Add files via upload

---
+++ x.py
@@ -0,0 +1,9 @@
+for i in 'u r cute':
+    print(i * 10, end='')
+for i in 'u r cute ':
+    if i == 'p':
+       break
+else:
+    print(' There are no cute people left')
+    print('i was joking')
+    print ('goodluck!')
